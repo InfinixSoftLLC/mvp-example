4f5a69fd208cc04a51f7e9f72640997b9d339f07
Fernando Ortiz <ortizfernandomartin@gmail.com>
2016-10-23 19:44:39 -0300

    Initial Commit

---
+++ MVPExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVPExample
+//
+//  Created by Fernando Ortiz on 10/23/16.
+//  Copyright © 2016 Fernando Martín Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
